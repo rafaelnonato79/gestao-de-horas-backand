9c31defc6f7a9687d087ea7604716c59f2aa0fe3
Rafael Nonato <rafaelnonato79@gmail.com>
2024-01-15 20:56:38 -0300

    está funcionando

--- src/main/java/com/gestaohoras/gestaohoras/repository/AtividadeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gestaohoras.gestaohoras.repository;
-
-import com.gestaohoras.gestaohoras.model.Atividade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AtividadeRepository extends JpaRepository<Atividade, Integer> {
-}
